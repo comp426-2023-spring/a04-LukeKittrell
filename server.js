022c84759764da9ab5858bec7f56d930fb0bdc3a
Luke Kittrell <lukekitt@ad.unc.edu>
2023-03-29 23:33:44 -0400

    basic setup

---
+++ server.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import minimist from "minimist";
+import express from "express";
+import {rps, rpsls} from "./lib/rpsls.js";
+
+const args = minimist(process.argv.slice(2));
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
