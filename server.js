23ea5bb9982c741d2e5f435d2a90814f98639a96
Luke Kittrell <lukekitt@ad.unc.edu>
2023-03-29 23:45:27 -0400

    check basic and first endpoint. start server

--- server.js
+++ server.js
@@ -9,3 +9,20 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+const PORT = args.port || 5000;
+
+//check /app endpoint
+app.get('/app/', (rep, res) => {
+	res.status(200).send('200 OK');
+});
+
+//default endpoint
+app.get('*', rep, res) => {
+	res.status(404).send('404 NOT FOUND');
+});
+
+//start server
+app.listen(PORT, () => {
+	console.log('sever running on port ${PORT}');
+});
